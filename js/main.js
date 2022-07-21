6bc5326b364e515464aa8c3a4f5f07625b226d06
Kamol21 <kamolakangdev@gmail.com>
2022-07-21 17:20:55 +0500

    add faq and contacts page and fix all problems

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 var elModalOpenButton1 = document.querySelector(".header-end__last-link");
-var elModalOpenButton2 = document.querySelector(".faq-s1__button");
 var elModal = document.querySelector(".modal");
 
 elModalOpenButton1.addEventListener("click", function(){
   elModal.classList.add("modal-show");
 })
-
-elModalOpenButton2.addEventListener("click", function(){
-  elModal.classList.add("modal-show");
-})
-
 
 var elModalCloseButton = document.querySelector(".modal-close-button");
 
